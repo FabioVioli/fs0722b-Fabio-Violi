af958edecf0333f2fdffb0e6c9f4d231772f69bb
FabioVioli <fabiovioli84@gmail.com>
2022-10-31 16:55:06 +0100

    esercizio w2-d1

---
+++ b/m1/w2/d1/sito1/script.js
@@ -0,0 +1,8 @@
+//alert('Hello World!')
+
+/* mettere // disattiva la riga, permettendoti di scrivere in quella dopo */
+
+document.getElementById('test').innerHTML = 'Hello World'
+
+/* Camelcase: quando ci sono più elementi attaccati, la prima lettera di ogni parola è maiuscola*/
+
