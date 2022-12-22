4ce4621be1a693dbdd853c199c52c177ed0555c7
FabioVioli <fabiovioli84@gmail.com>
2022-12-22 16:41:23 +0100

    ex

--- b/m3/w1/d4/exercise/assets/scripts/exercise.ts
+++ b/m3/w1/d4/exercise/assets/scripts/exercise.ts
@@ -12,23 +12,23 @@
 
 fetch('../exercise/assets/json/Abbigliamento.json')
     .then(response => response.json())
-    .then(result =>{
+    .then(result => {
 
         class Item {
-            id:number;
-            codprod:number;
-            collezione:string;
-            capo:string;
-            modello:number;
-            quantità:number;
-            colore:string;
-            prezzoivaesclusa:number;
-            prezzoivainclusa:number;
-            disponibile:string;
-            saldo:number;
-            immagine:string;
+            id: number;
+            codprod: number;
+            collezione: string;
+            capo: string;
+            modello: number;
+            quantità: number;
+            colore: string;
+            prezzoivaesclusa: number;
+            prezzoivainclusa: number;
+            disponibile: string;
+            saldo: number;
+            immagine: string;
 
-            constructor (id:number,codprod:number,collezione:string,capo:string,modello:number,quantità:number,colore:string,prezzoivaesclusa:number,prezzoivainclusa:number,disponibile:string,saldo:number,immagine:string){
+            constructor(id: number, codprod: number, collezione: string, capo: string, modello: number, quantità: number, colore: string, prezzoivaesclusa: number, prezzoivainclusa: number, disponibile: string, saldo: number, immagine: string) {
 
                 this.id = id;
                 this.codprod = codprod;
@@ -44,40 +44,43 @@ fetch('../exercise/assets/json/Abbigliamento.json')
                 this.immagine = immagine;
             }
 
-            getSaldoCapo():any{
-               return ((this.prezzoivainclusa/100)*this.saldo).toFixed(2)
-                
+            getSaldoCapo(): any {
+                return ((this.prezzoivainclusa / 100) * this.saldo).toFixed(2)
+
             }
-            getAcquistoCapo():any{
+            getAcquistoCapo(): any {
                 let prezzoFinale = (this.prezzoivainclusa - this.getSaldoCapo()).toFixed(2)
                 return prezzoFinale
             }
-        }        
+
+        }
         result.forEach(element => {
-            
-            let Capo = new Item (element.id, element.codprod, element.collezione,element.capo,element.modello,element.quantità,element.colore,element.prezzoivaesclusa,element.prezzoivainclusa,element.disponibile,element.saldo,element.immagine)
+
+            let Capo = new Item(element.id, element.codprod, element.collezione, element.capo, element.modello, element.quantità, element.colore, element.prezzoivaesclusa, element.prezzoivainclusa, element.disponibile, element.saldo, element.immagine)
             console.log(`Dettagli ${element.capo}:`, Capo)
-            console.log(`Prezzo iniziale: `+ element.prezzoivainclusa + `€`)
-            console.log(`Percentuale di sconto su ${element.capo}: `+ Capo.getSaldoCapo()+"%")
-            console.log(`Costo finale ${element.capo}: `+ Capo.getAcquistoCapo()+"€")
+            console.log(`Prezzo iniziale: ` + element.prezzoivainclusa + `€`)
+            console.log(`Percentuale di sconto su ${element.capo}: ` + Capo.getSaldoCapo() + "%")
+            console.log(`Costo finale ${element.capo}: ` + Capo.getAcquistoCapo() + "€")
             console.log("-----------------------------------------------------------")
 
 
             let parent = (<HTMLDivElement>document.getElementById("parent"))
             parent.innerHTML += `<div class="card col-3 g-2 mx-5">
-            <div class="card-body d-flex flex-column justify-content-evenly align-items-center">
+            <div class="card-body d-flex flex-column justify-content-evenly align-items-center" id="btn-append">
               <img src="${element.immagine}" alt="">
               <h5 class="card-title">${element.capo}</h5>
-              <p class="card-text">Prezzo iniziale ${element.prezzoivainclusa} €</p>
-              <p class="card-text">Sconto Black Friday ${element.saldo}%</p>
-              <p>Risparmi ${Capo.getSaldoCapo()}</p>
-              <p class="card-text">Prezzo finale ${Capo.getAcquistoCapo()}€</p>
+              <p class="card-text">Prezzo iniziale: ${element.prezzoivainclusa} €</p>
+              <p class="card-text">Sconto Black Friday: ${element.saldo}%</p>
+              <p>Risparmi: ${Capo.getSaldoCapo()}</p>
+              <p class="card-text">Prezzo finale: ${Capo.getAcquistoCapo()}€</p>
               <button>Aggiungi al carrello</button>
             </div>
           </div>`
+
+
         });
-        
-        });
+
+    });
 
 
 /* 
@@ -88,4 +91,4 @@ fetch('../exercise/assets/json/Abbigliamento.json')
 ██║     ██║     ██╔══██║╚════██║╚════██║    ██║   ██║██║   ██║   ██║   ╚════██║██║██║  ██║██╔══╝      ██╔══╝  ██╔══╝     ██║   ██║     ██╔══██║            
 ╚██████╗███████╗██║  ██║███████║███████║    ╚██████╔╝╚██████╔╝   ██║   ███████║██║██████╔╝███████╗    ██║     ███████╗   ██║   ╚██████╗██║  ██║            
  ╚═════╝╚══════╝╚═╝  ╚═╝╚══════╝╚══════╝     ╚═════╝  ╚═════╝    ╚═╝   ╚══════╝╚═╝╚═════╝ ╚══════╝    ╚═╝     ╚══════╝   ╚═╝    ╚═════╝╚═╝  ╚═╝            
-                                                                                                                                                                                            */ 
+                                                                                                                                                                                            */
